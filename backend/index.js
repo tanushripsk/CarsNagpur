356fdffa199f511d05cedab1ae84a64d54bc8109
tanushripsk <psktanushri22dhote@gmail.com>
2024-11-30 11:29:26 +0530

    ip change

--- backend/index.js
+++ backend/index.js
@@ -5,19 +5,20 @@ const cors = require('cors');
 const { connectDB } = require('./db.js');
 const userRoutes = require('./routes/AllRoutes.js');
 const carsRoutes = require('./routes/carRoutes.js');
-const AuthRoutes = require('./routes/Auth.js'); // Import the auth routes
-const TestDriveRoutes = require ('./routes/testDriveRoutes.js')
+const AuthRoutes = require('./routes/Auth.js');
+const TestDriveRoutes = require('./routes/testDriveRoutes.js');
 const path = require('path');
+
 const app = express();
 const port = 3000;
 
 // Connect to the database
 connectDB();
 
-// Enable CORS and allow requests from your React app's origin
-// const allowedOrigins = ['http://65.0.32.220:3001', 'http://65.0.32.220:5173']; // Add more origins if needed
-const allowedOrigins = ['http://3.111.58.199:3001']; // Add more origins if needed
+// Allowed origins for CORS
+const allowedOrigins = ['http://3.111.58.199:3001'];  // Add more origins if needed
 
+// Enable CORS with custom configuration
 app.use(cors({
     origin: function (origin, callback) {
         if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
@@ -27,24 +28,25 @@ app.use(cors({
         }
     },
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    credentials: true
+    credentials: true,  // Allows cookies to be sent
 }));
 
 // Middleware to parse JSON
 app.use(express.json());
-app.use(cors());                //tanushri edit
-app.use(express.static(path.join(__dirname, "../build")));    //tanushri edit
+
+// Serve static files (React build folder)
+app.use(express.static(path.join(__dirname, "../build")));
+
 // Use routes
 app.use('/api/users', userRoutes);
 app.use('/api/cars', carsRoutes);
-app.use('/auth', AuthRoutes); // Use the auth routes
-app.use('/api/testDrive', TestDriveRoutes)
+app.use('/auth', AuthRoutes);
+app.use('/api/testDrive', TestDriveRoutes);
 
-
+// Fallback for serving the React app (in production mode)
 app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../build", "index.html"));    //tanushri edit
-  });
-
+    res.sendFile(path.join(__dirname, "../build", "index.html"));
+});
 
 // Start the server
 app.listen(port, () => {
