04c8915a26d2a3b3001bc262cf1cd5f1f7d0219b
tanushripsk <psktanushri22dhote@gmail.com>
2024-12-07 10:33:56 +0530

    carsnagpur project

--- backend/db.js
+++ backend/db.js
@@ -4,7 +4,7 @@ const mongoose= require ('mongoose')
 
 const connectDB=async ()=>{
     try {
-        await mongoose.connect('mongodb+srv://madhupreamcateres:4RghgFlfvBEGyspk@deploy.eyw7q.mongodb.net/deployecar')
+        await mongoose.connect('mongodb+srv://abhishek9aws:BjCGIrc9hH6KLyCB@success.14mfd.mongodb.net/')
         console.log('mongo connected');
         
     } catch (error) {
